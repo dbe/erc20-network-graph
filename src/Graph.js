978f0ad6bb71792b0ccbadf3a97c9526baeca383
dbe <davidbrianethier@gmail.com>
2019-01-30 16:38:33 -0700

    Just commenting

--- src/Graph.js
+++ src/Graph.js
@@ -5,7 +5,8 @@ import Web3 from 'web3';
 import './Graph.css';
 
 import ABI from "./contractAbis/Burner.abi.js"
-let ADDRESS = '0xDec31651Bec1fBbFF392aa7DE956d6EE4559498b'
+// let ADDRESS = '0xDec31651Bec1fBbFF392aa7DE956d6EE4559498b'
+let ADDRESS = '0xA95D505E6933cB790ED3431805871EfE4E6BbafD'
 
 class Graph extends Component {
   constructor(props) {
@@ -13,7 +14,11 @@ class Graph extends Component {
 
     this.web3 = new Web3(new Web3.providers.HttpProvider('https://dai.poa.network'))
 
-    this.fetchPastEvents()
+    this.fetchPastEvents().then(data => {
+      console.log('data: ', data);
+      let div = document.getElementById('network')
+      let network = new vis.Network(div, data, {});
+    });
   }
 
   fetchPastEvents() {
@@ -26,16 +31,10 @@ class Graph extends Component {
       toBlock: 'latest'
     })
 
-    console.log('past: ', past);
-
-    past.then(events => {
+    return past.then(events => {
       console.log('events: ', events);
       events.forEach((event, i) => {
         let { to, from, value } = event.returnValues
-        // console.log('to: ', to);
-        // console.log('from: ', from);
-        // console.log('value: ', value);
-
         nodeSet.add(to)
         nodeSet.add(from)
 
@@ -49,8 +48,15 @@ class Graph extends Component {
         })
       );
 
+      edges = new vis.DataSet(edges)
+
       console.log('nodes: ', nodes);
       console.log('edges: ', edges);
+
+      return {
+        nodes,
+        edges
+      }
     }).catch(e => {
       console.log('e: ', e);
     })
@@ -58,7 +64,7 @@ class Graph extends Component {
 
   render() {
     return (
-      <div>
+      <div id="network">
       </div>
     )
   }
